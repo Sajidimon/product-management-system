21c482002c3a01c680a9854fd312ed3006bd0b03
sajidimon <imonchowdhury6@gmail.com>
2024-11-08 16:37:51 +0600

    issue fixed & project completed

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
-use Illuminate\Container\Attributes\Storage;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class ProductController extends Controller
 {
